dcce11016e5d3464ee84e1a7344c23dd32712b14
Ang107 <kosukenabeta1007@gmail.com>
2025-04-30 20:31:28 +0900

    ボディサイズの上限引き上げ

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // ここで experimental 以下に serverActions の設定を追加
+  experimental: {
+    serverActions: {
+      // ボディ上限を 25MB に設定（必要に応じて調整）
+      bodySizeLimit: 250 * 1024 * 1024,
+    },
+  },
 };
 
 export default nextConfig;
