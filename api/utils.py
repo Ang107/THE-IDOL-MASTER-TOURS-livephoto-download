6a8f0bb3001ce4327d8f16d4d40f79a81912c71e
Ang107 <kosukenabeta1007@gmail.com>
2025-05-08 22:39:49 +0900

    htmlを解析して取得した画像ファイル名を用いて画像取得を行うように変更

--- api/utils.py
+++ api/utils.py
@@ -1,16 +1,45 @@
-# utils.py
 from collections import OrderedDict
 import datetime as dt
 from zoneinfo import ZoneInfo
-import aiohttp, io, zipfile, os, time, asyncio
+import aiohttp, io, zipfile, os, asyncio, re
+from html.parser import HTMLParser
+from urllib.parse import urljoin
 
-
-BASE = "https://livephoto.idolmaster-tours-w.bn-am.net/livephoto/{code}/{n}.jpeg"
-
+# ──────────────────── 定数 ────────────────────
+BASE_PAGE = "https://livephoto.idolmaster-tours-w.bn-am.net/livephoto/{code}/"
 _img_cache: OrderedDict[str, list[bytes]] = OrderedDict()
 MAX_ITEMS = 100
 
 
+# ──────────────────── HTML パーサ ───────────────
+class _ImgSrcParser(HTMLParser):
+    pattern = re.compile(r"\.(jpe?g|png)$", re.I)
+
+    def __init__(self):
+        super().__init__()
+        self.srcs: list[str] = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag.lower() != "img":
+            return
+        attrs = dict(attrs)
+        src = attrs.get("src")
+        if src and self.pattern.search(src):
+            self.srcs.append(src.lstrip("./"))
+
+
+async def _get_img_names(session: aiohttp.ClientSession, code: str) -> list[str]:
+    page_url = BASE_PAGE.format(code=code)
+    async with session.get(page_url) as r:
+        if r.status != 200:
+            return []
+        html = await r.text()
+    parser = _ImgSrcParser()
+    parser.feed(html)
+    return parser.srcs
+
+
+# ──────────────────── 画像取得 ────────────────
 async def _fetch_one(session, url):
     async with session.get(url) as r:
         return await r.read() if r.status == 200 else None
@@ -18,11 +47,12 @@ async def _fetch_one(session, url):
 
 async def fetch_all(code: str) -> list[bytes]:
     if code in _img_cache:
-        imgs = _img_cache.get(code)
-        return imgs
+        return _img_cache[code]
 
     async with aiohttp.ClientSession() as s:
-        tasks = [_fetch_one(s, BASE.format(code=code, n=i)) for i in range(3)]
+        names = await _get_img_names(s, code)
+        page_url = BASE_PAGE.format(code=code)
+        tasks = [_fetch_one(s, urljoin(page_url, n)) for n in names]
         imgs = await asyncio.gather(*tasks)
         imgs = [b for b in imgs if b]
 
@@ -32,8 +62,8 @@ async def fetch_all(code: str) -> list[bytes]:
     return imgs
 
 
+# ──────────────────── その他ユーティリティ ───
 def timestamp() -> str:
-    """日本時間 (JST) のタイムスタンプを返す"""
     jst = ZoneInfo("Asia/Tokyo")
     return dt.datetime.now(jst).strftime("%Y_%m_%d_%H_%M_%S")
 
